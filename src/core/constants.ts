f1d56874a35ad97a7fc9274fdec64da5b35388f3
aymantaybi <42820584+aymantaybi@users.noreply.github.com>
2023-01-19 18:37:55 +0100

    fix: use http over https for HOST & WEBHOST

--- src/core/constants.ts
+++ src/core/constants.ts
@@ -16,5 +16,5 @@ export const BLOKS_VERSION_ID = '1b030ce63a06c25f3e4de6aaaf6802fe1e76401bc5ab6e5
 
 export const HOSTNAME = process.env.IG_HOSTNAME || 'i.instagram.com';
 export const WEB_HOSTNAME = process.env.IG_WEB_HOSTNAME || 'www.instagram.com';
-export const HOST = `https://${HOSTNAME}/`;
-export const WEBHOST = `https://${WEB_HOSTNAME}/`;
+export const HOST = `http://${HOSTNAME}/`;
+export const WEBHOST = `http://${WEB_HOSTNAME}/`;
